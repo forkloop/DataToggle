062a7a71027674e49512f669f7742408cb29c201
forkloop <forkloop@gmail.com>
2012-10-30 23:14:02 -0400

    Refactor the code.

--- src/us/forkloop/datatoggle/DataToggleWidget.java
+++ src/us/forkloop/datatoggle/DataToggleWidget.java
@@ -1,7 +1,6 @@
 package us.forkloop.datatoggle;
 
 import java.lang.reflect.Method;
-
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -16,8 +15,11 @@ public class DataToggleWidget extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Method dataConnSwitchmethod, getITelephonyMethod;
-        Class telephonyManagerClass, ITelephonyClass;
+        super.onReceive(context, intent);
+
+        Method dataConnSwitchMethod, getITelephonyMethod;
+        Class<? extends Object> telephonyManagerClass;
+        Class<? extends Object> ITelephonyClass;
         Object ITelephonyStub;
         RemoteViews rv;
         AppWidgetManager wm;
@@ -25,68 +27,47 @@ public class DataToggleWidget extends AppWidgetProvider {
         boolean isEnabled;
 
         rv = new RemoteViews(context.getPackageName(), R.layout.widget);
-
         if (intent.getAction().equals("us.forkloop.datatoggle.TOGGLE")) {
-            // http://stackoverflow.com/questions/3644144/how-to-disable-mobile-data-on-android
+            /**
+             * @link http://stackoverflow.com/questions/3644144/how-to-disable-mobile-data-on-android
+             */
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             if (telephonyManager.getDataState() == TelephonyManager.DATA_CONNECTED) {
                 isEnabled = true;
-                Log.i("log", "enabled");
+                Log.d(getClass().getSimpleName(), "DATA_CONNECTED");
             } else {
                 isEnabled = false;
-                Log.d("log", "disabled");
+                Log.d(getClass().getSimpleName(), "DATA_DISCONNECTED or");
             }
             try {
-                telephonyManagerClass = Class.forName(telephonyManager.getClass().getName());
-                // Method getITelephonyMethod;
+                telephonyManagerClass = TelephonyManager.class;
                 getITelephonyMethod = telephonyManagerClass.getDeclaredMethod("getITelephony");
                 getITelephonyMethod.setAccessible(true);
                 ITelephonyStub = getITelephonyMethod.invoke(telephonyManager);
                 ITelephonyClass = Class.forName(ITelephonyStub.getClass().getName());
                 if (isEnabled) {
-                    dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("disableDataConnectivity");
-                    Log.i("log", "try to disable...");
-                    rv.setImageViewResource(R.id.imgButton, R.drawable.princess);
+                    dataConnSwitchMethod = ITelephonyClass.getDeclaredMethod("disableDataConnectivity");
+                    Log.d(getClass().getSimpleName(), "user try to disable connection...");
+                    rv.setImageViewResource(R.id.image_button, R.drawable.princess);
                 } else {
-                    dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("enableDataConnectivity");
-                    Log.i("log", "try to enable...");
-                    rv.setImageViewResource(R.id.imgButton, R.drawable.emerald);
+                    dataConnSwitchMethod = ITelephonyClass.getDeclaredMethod("enableDataConnectivity");
+                    Log.d(getClass().getSimpleName(), "user try to enable connection...");
+                    rv.setImageViewResource(R.id.image_button, R.drawable.emerald);
                 }
-                dataConnSwitchmethod.setAccessible(true);
-                dataConnSwitchmethod.invoke(ITelephonyStub);
+                dataConnSwitchMethod.setAccessible(true);
+                dataConnSwitchMethod.invoke(ITelephonyStub);
             } catch (Exception e) {
-                Log.i("log", e.toString());
+                e.printStackTrace();
+                Log.d(getClass().getSimpleName(), e.toString());
             }
         } else if (intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
-            Log.i("log", "connection changed");
             TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             if (telephonyManager.getDataState() == TelephonyManager.DATA_CONNECTED) {
-                isEnabled = true;
-                Log.i("log", "enabled");
+                rv.setImageViewResource(R.id.image_button, R.drawable.emerald);
+                Log.d(getClass().getSimpleName(), "Connection enabled.");
             } else {
-                isEnabled = false;
-                Log.i("log", "disabled");
-            }
-            try {
-                telephonyManagerClass = Class.forName(telephonyManager.getClass().getName());
-                // Method getITelephonyMethod;
-                getITelephonyMethod = telephonyManagerClass.getDeclaredMethod("getITelephony");
-                getITelephonyMethod.setAccessible(true);
-                ITelephonyStub = getITelephonyMethod.invoke(telephonyManager);
-                ITelephonyClass = Class.forName(ITelephonyStub.getClass().getName());
-                if (isEnabled) {
-                    dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("enableDataConnectivity");
-                    Log.i("log", "system try to enable...");
-                    rv.setImageViewResource(R.id.imgButton, R.drawable.emerald);
-                } else {
-                    dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("disableDataConnectivity");
-                    Log.i("log", "system try to disable...");
-                    rv.setImageViewResource(R.id.imgButton, R.drawable.princess);
-                }
-                dataConnSwitchmethod.setAccessible(true);
-                dataConnSwitchmethod.invoke(ITelephonyStub);
-            } catch (Exception e) {
-                Log.i("log", e.toString());
+                rv.setImageViewResource(R.id.image_button, R.drawable.princess);
+                Log.i(getClass().getSimpleName(), "Connection disabled.");
             }
         }
         wm = AppWidgetManager.getInstance(context);
@@ -94,7 +75,6 @@ public class DataToggleWidget extends AppWidgetProvider {
         int[] allWidgetIds = wm.getAppWidgetIds(widgetPro);
         for (int i = 0; i < allWidgetIds.length; i++)
             wm.updateAppWidget(allWidgetIds[i], rv);
-        super.onReceive(context, intent);
     }
 
     @Override
@@ -104,7 +84,7 @@ public class DataToggleWidget extends AppWidgetProvider {
             Intent toggleIntent = new Intent("us.forkloop.datatoggle.TOGGLE");
             PendingIntent pendingToggle = PendingIntent.getBroadcast(context, 0, toggleIntent, 0);
             RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
-            rv.setOnClickPendingIntent(R.id.imgButton, pendingToggle);
+            rv.setOnClickPendingIntent(R.id.image_button, pendingToggle);
             appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
         }
     }
